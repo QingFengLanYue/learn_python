423f13e04a93409aa2e4a6882e7e7805c2e53858
luokesasi <919880391@qq.com>
2021-05-06 14:06:44 +0800

    feiji

--- test01/excel文件处理.py
+++ test01/excel文件处理.py
@@ -1,21 +1,22 @@
 from test01.文件差异比较 import dealExceldiff
 import pandas as pd
-# file1 = "D:/work2/test1.xlsx"
-# file2 = "D:/work2/test2.xlsx"
-# file_write = "D:/work2/test_diff.xlsx"
+file1 = "D:/work2/test1.xlsx"
+file2 = "D:/work2/test2.xlsx"
+file_write = "D:/work2/test_diff.xlsx"
+primary_key='hh'
+
+if __name__ == '__main__':
+    dealExceldiff.deal_result(primary_key,file1, file2, file_write)
+
+# file3 = "D:/work2/test.xlsx"
+# df=pd.read_excel(file3,sheet_name='a',index_col=0)
+# print(df)
 #
-# # if __name__ == '__main__':
-# #     dealExceldiff.deal_result(file1, file2, file_write)
-
-file3 = "D:/work2/test.xlsx"
-df=pd.read_excel(file3,sheet_name='a',index_col=0)
-print(df)
-
-writer = pd.ExcelWriter(file3)
-
-df1 = pd.DataFrame([[1,2],['a','b'],[3,4]],columns=['id','name'])
-df2 = pd.DataFrame(['b'])
-
-df1.to_excel(writer, sheet_name='a')
-df2.to_excel(writer, sheet_name='b')
-writer.save()
+# writer = pd.ExcelWriter(file3)
+#
+# df1 = pd.DataFrame([[1,2],['a','b'],[3,4]],columns=['id','name'])
+# df2 = pd.DataFrame(['b'])
+#
+# df1.to_excel(writer, sheet_name='a')
+# df2.to_excel(writer, sheet_name='b')
+# writer.save()
--- test01/文件差异比较.py
+++ test01/文件差异比较.py
@@ -4,17 +4,18 @@ import numpy as np
 
 class dealExceldiff:
 
-    def deal_same(df1,df2):
+        
+    def deal_same(primary_key,df1,df2):
         c=df1.columns.values
-        lo=df1['id'].values
+        lo=df1[primary_key].values
         co=df1.shape[1]
         m=[]
         for i in lo:
-            f1=df1.loc[df1['id']==i].values.flatten()
-            f2=df2.loc[df2['id']==i].values.flatten()
+            f1=df1.loc[df1[primary_key]==i].values.flatten()
+            f2=df2.loc[df2[primary_key]==i].values.flatten()
             for j in range(co):
                 if f1[j] != f2[j]:
-                    print('修改了id=%s行对应的%s列,%s->%s'%(i,c[j],f1[j],f2[j]))
+                    print('修改了%s=%s行对应的%s列,%s->%s'%(primary_key,i,c[j],f1[j],f2[j]))
                     f1[j]=str(f1[j])+'->'+str(f2[j])
                 else :
                     f1[j] = f1[j]
@@ -36,20 +37,20 @@ class dealExceldiff:
         df['flag'] = -1
         return df
 
-    def add_del(df1,df2):
-        df_same1=df1.loc[df1['id'].isin(df2['id'])]
-        df_same2=df2.loc[df2['id'].isin(df1['id'])]
-        df_add=df2.loc[~df2['id'].isin(df1['id'])]
-        df_del=df1.loc[~df1['id'].isin(df2['id'])]
+    def add_del(primary_key,df1,df2):
+        df_same1=df1.loc[df1[primary_key].isin(df2[primary_key])]
+        df_same2=df2.loc[df2[primary_key].isin(df1[primary_key])]
+        df_add=df2.loc[~df2[primary_key].isin(df1[primary_key])]
+        df_del=df1.loc[~df1[primary_key].isin(df2[primary_key])]
         return df_same1, df_same2, df_add, df_del
 
-    def deal_result(file1,file2,file_write):
+    def deal_result(primary_key,file1,file2,file_write):
         df1 = pd.read_excel(file1)
         df2 = pd.read_excel(file2)
 
-        df_same1,df_same2,df_add,df_del=dealExceldiff.add_del(df1,df2)
+        df_same1,df_same2,df_add,df_del=dealExceldiff.add_del(primary_key,df1,df2)
 
-        deal_df1 = dealExceldiff.deal_same(df_same1, df_same2)
+        deal_df1 = dealExceldiff.deal_same(primary_key,df_same1, df_same2)
         deal_df2 = dealExceldiff.deal_add(df_add)
         deal_df3 = dealExceldiff.deal_del(df_del)
         res=pd.concat([deal_df1,deal_df2,deal_df3],ignore_index=True)
