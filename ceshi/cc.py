d75e2fe09f2c2e449c23da8ec1e4a00f77882301
luokesasi <919880391@qq.com>
2021-08-17 18:51:32 +0800

    文本处理

--- ceshi/cc.py
+++ ceshi/cc.py
@@ -5,19 +5,23 @@ import random
 import pandas as pd
 import os
 
+
 def red_qiu():
     list = []
     while len(list) != 6:
-        red=random.randint(1,32)
+        red = random.randint(1, 32)
         if red not in list:
             list.append(red)
     return list
 
+
 def blue_qiu():
-    blue=random.randint(1,16)
+    blue = random.randint(1, 16)
     return blue
-def win(df2,win_num):
-    #01 04 12 20 25 32 02
+
+
+def win(df2, win_num):
+    # 01 04 12 20 25 32 02
     '''
      中0-2个红球1个蓝球是六等奖—5元。中3个红球1个蓝球或4个红球是五等奖—10元。
 
@@ -26,55 +30,55 @@ def win(df2,win_num):
     只中6个红球是二等奖—最高500万元。全部猜中为一等奖—最高1000万元。
 
     '''
-    first1, two1, three1, four1, five1, six1 =  0, 0, 0, 0, 0, 0
-    #df2 = pd.read_csv(file)
+    first1, two1, three1, four1, five1, six1 = 0, 0, 0, 0, 0, 0
+    # df2 = pd.read_csv(file)
     for indexs in df2.index:
-        red=0
-        blue=0
+        red = 0
+        blue = 0
         for red_num in df2.loc[indexs].values[0:6]:
-            #print(red_num)
+            # print(red_num)
             if red_num in win_num[0:6]:
-                red+=1
+                red += 1
 
         for blue_num in df2.loc[indexs].values[6:7]:
             if blue_num == win_num[6]:
-                blue=1
-        first,two,three,four,five,six=jiangxiang(red,blue)
-        if first == 1 :
-            print('恭喜第:%s名用户斩获一等奖:%s'%(indexs,df2.loc[indexs].values))
-            first1+=1
-        elif two == 1 :
-            print('恭喜第:%s名用户斩获二等奖:%s'%(indexs,df2.loc[indexs].values))
-            two1+=1
-        elif three == 1 :
-            print('恭喜第:%s名用户斩获三等奖:%s'%(indexs,df2.loc[indexs].values))
-            three1+=1
-        elif four == 1 :
-            four1+=1
-        elif five == 1 :
-            five1+=1
-        elif six == 1 :
-            six1+=1
+                blue = 1
+        first, two, three, four, five, six = jiangxiang(red, blue)
+        if first == 1:
+            print('恭喜第:%s名用户斩获一等奖:%s' % (indexs, df2.loc[indexs].values))
+            first1 += 1
+        elif two == 1:
+            print('恭喜第:%s名用户斩获二等奖:%s' % (indexs, df2.loc[indexs].values))
+            two1 += 1
+        elif three == 1:
+            print('恭喜第:%s名用户斩获三等奖:%s' % (indexs, df2.loc[indexs].values))
+            three1 += 1
+        elif four == 1:
+            four1 += 1
+        elif five == 1:
+            five1 += 1
+        elif six == 1:
+            six1 += 1
     return first1, two1, three1, four1, five1, six1
 
 
+def jiangxiang(red, blue):
+    first, two, three, four, five, six = 0, 0, 0, 0, 0, 0
+    if red <= 2 and blue == 1:
+        six = 1
+    elif (red == 3 and blue == 1) or (red == 4 and blue == 0):
+        five = 1
+    elif (red == 4 and blue == 1) or (red == 5 and blue == 0):
+        four = 1
+    elif red == 5 and blue == 1:
+        three = 1
+    elif red == 6 and blue == 0:
+        two = 1
+    elif red == 6 and blue == 1:
+        first = 1
 
-def jiangxiang(red,blue):
-    first, two, three, four, five, six=0,0,0,0,0,0
-    if red <= 2 and blue == 1 :
-        six=1
-    elif (red == 3 and blue == 1) or (red == 4 and blue ==0 ):
-        five=1
-    elif (red == 4 and blue == 1) or (red == 5 and blue ==0 ):
-        four=1
-    elif (red == 5 and blue == 1) :
-        three=1
-    elif (red == 6 and blue == 0) :
-        two=1
-    elif (red == 6 and blue == 1) :
-        first=1
+    return first, two, three, four, five, six
 
-    return first,two,three,four,five,six
 
 def win_num_random():
     red = sorted(red_qiu())
@@ -83,52 +87,61 @@ def win_num_random():
     return red
 
 
-def anay_cc(g_num,win_num):
+# def write_csv(csv_file):
+#     columns = ['一等奖', '二等奖', '三等奖', '四等奖', '五等奖', '六等奖', '总花费', '总奖金']
+#     mx = pd.Series([first1, two1, three1, four1, five1, six1, totol_win, totol_money], columns=columns)
+#     df.to_csv(csv_file, mode='a', header=False)
+
+def anay_cc(m, g_num, win_num, csv_file):
     li = []
     for i in range(g_num):
-        red=sorted(red_qiu())
-        blue=blue_qiu()
+        red = sorted(red_qiu())
+        blue = blue_qiu()
         red.append(blue)
 
         li.append(red)
 
-    order = ['1', '2','3','4','5','6','7']
-    df = pd.DataFrame(li,columns=order)
-    print('开始写入文件。。。')
-    #df.to_csv(file,index=False)
-    print('开始分析文件。。。')
-    print('-----------------------------------------------------------')
-    first1,two1,three1,four1,five1,six1=win(df,win_num)
-    totol_win=six1*5+five1*10+four1*200+three1*3000+two1*291499+first1*5000000
-    totol_money=g_num*2
-    hbl=totol_win/totol_money*100
-    print('-----------------------------------------------------------')
+    order = ['1', '2', '3', '4', '5', '6', '7']
+    df = pd.DataFrame(li, columns=order)
+    # print('开始写入文件。。。')
+    # # df.to_csv(file,index=False)
+    # print('开始分析文件。。。')
+    # print('-----------------------------------------------------------')
+    first1, two1, three1, four1, five1, six1 = win(df, win_num)
+    totol_win = six1 * 5 + five1 * 10 + four1 * 200 + three1 * 3000 + two1 * 291499 + first1 * 5000000
+    totol_money = g_num * 2
+    hbl = totol_win / totol_money * 100
+    # print('-----------------------------------------------------------')
     print('一等奖:%s\n二等奖:%s\n三等奖:%s\n四等奖:%s\n五等奖:%s\n六等奖:%s'
-          % (first1, two1, three1, four1, five1, six1 ))
-    print('总花费为：%s元' %(totol_money))
-    print('总奖金为：%s元'%(totol_win))
-    print('回报率为:%.2f%%'%hbl)
+          % (first1, two1, three1, four1, five1, six1))
+    print('总花费为：%s元' % (totol_money))
+    print('总奖金为：%s元' % (totol_win))
+    print('回报率为:%.2f%%' % hbl)
+    ll = [m, win_num, g_num, first1, two1, three1, four1, five1, six1, totol_money, totol_win, hbl]
+    # columns=['次数','中奖号码','一等奖','二等奖','三等奖','四等奖','五等奖','六等奖', '总花费', '总收益','回报率']
+    csv_write = pd.DataFrame([ll])
+    csv_write.to_csv(csv_file, mode='a', header = 0, index = 0, float_format='%.2f')
     return hbl
     # win_num = [1, 4, 12, 20, 25, 32, 2]
     # order = ['1', '2', '3', '4', '5', '6', '7']
     # df = pd.DataFrame([[1,4,12,20,25,32,4],[1,4,12,20,25,32,2]], columns=order)
     # win(df,win_num=[1,4,12,20,25,32,2])
 
-def cs():
-    #win_num=[5,6,14,16,19,27,10]
-    win_num=win_num_random()
-    print('此次随机的奖号码为：%s'%win_num)
-    g_num=10000
-    file='D:/work2/cc.csv'
 
-    hbl=anay_cc(g_num,win_num)
+def cs(i,g_num, csv_file):
+    # win_num=[5,6,14,16,19,27,10]
+    win_num = win_num_random()
+    print('此次随机的奖号码为：%s' % win_num)
+    g_num = g_num
+    # file='D:/work2/cc.csv'
+
+    hbl = anay_cc(i, g_num, win_num, csv_file)
     return hbl
+
 
 def delete_line(lines):
     w = open(file, 'w', encoding='utf-8')
-
     w.writelines([item for item in lines[:-1]])
-
     w.close()
 
 
@@ -141,14 +154,16 @@ def iter_delete_line(file):
             linecache.clearcache()
             a = linecache.getline(file, linecount)
             readFile.close()
-            compile = r'^\[.*\]$'
+            compile = r'^回报率为'
 
             if not re.search(compile, a):
                 delete_line(lines)
                 iter_delete_line(file)
 
+
 def file_size(file):
     return os.path.exists(file) and os.path.getsize(file)
+
 
 def con_deal(file):
     iter_delete_line(file)
@@ -158,28 +173,33 @@ def con_deal(file):
             lines = f1.readlines()
             last_line = lines[-1]  # 取最后一行
             # l=last_line.strip('[|]|').strip(' ').strip(']')
-            l = last_line.replace(']','').replace('[','').replace('\n','').strip(' ')
-            l1=[]
-            for i in l.split(', '):
-                l1.append(i)
-            l2=len(l1)+1
+            # l = last_line.replace(']', '').replace('[', '').replace('\n', '').strip(' ')
+            # l1 = []
+            # for i in l.split(', '):
+            #     l1.append(float(i))
+            # l2 = len(l1) + 1
+            l1 = []
+            l2 = re.match(r'.*开始(\d+)次测试.*', str(lines))
+            l2 = l2.group(1)
+            l2 = int(l2) + 1
     else:
         l1 = []
         l2 = 1
-    return l1,l2
+    return l1, l2
 
 
+if __name__ == '__main__':
 
-if __name__=='__main__':
-
-    file='D:/work2/cc.log'
+    file = 'D:/work2/cc.log'
+    csv_file = 'D:/work2/cc_analy.csv'
     sys.stdout = open(file, mode='a+', encoding='utf-8')
-    rest,i = con_deal(file)
+    rest, i = con_deal(file)
     while True:
-        print('开始%s次测试'%i)
-        print('#######################################################')
-        hbl=cs()
-        rest.append(round(hbl,2))
-        print(rest)
-        i+=1
+        print('\n')
+        print('开始%s次测试' % i)
+        hbl = cs(i,10000, csv_file)
+        # rest.append(round(hbl, 2))
+        # print(rest)
+        i += 1
+
         sys.stdout.flush()
--- ceshi/dealLog.py
+++ ceshi/dealLog.py
@@ -3,28 +3,108 @@ import os
 
 import numpy as np
 
-file='D:/work2/cc.log'
-with open(file,mode='r',encoding='UTF-8') as f:
-    lines=str(f.readlines())
-    pattern1 = re.compile('总花费为：(\d+)')
-    pattern2 = re.compile('总奖金为：(\d+)')
-    pattern3 = re.compile('回报率为:(\d+.?\d+)')
-    m1 = re.findall(pattern1,lines)
-    m2 = re.findall(pattern2,lines)
-    m3 = re.findall(pattern3,lines)
-    m1 = list(map(int, m1))
-    m2 = list(map(int, m2))
-    m3 = list(map(float, m3))
-    total_m1=sum(m1)
-    total_m2=sum(m2)
-    syl=round(total_m2/total_m1*100,2)
-    print('总奖金为:%s,总花费为:%s,收益率为:%s%%'%(total_m2,total_m1,syl))
+# file='D:/work2/cc.log'
+# with open(file,mode='r',encoding='UTF-8') as f:
+#     lines=str(f.readlines())
+#     pattern1 = re.compile('总花费为：(\d+)')
+#     pattern2 = re.compile('总奖金为：(\d+)')
+#     pattern3 = re.compile('回报率为:(\d+.?\d+)')
+#     m1 = re.findall(pattern1,lines)
+#     m2 = re.findall(pattern2,lines)
+#     m3 = re.findall(pattern3,lines)
+#     m1 = list(map(int, m1))
+#     m2 = list(map(int, m2))
+#     m3 = list(map(float, m3))
+#     total_m1=sum(m1)
+#     total_m2=sum(m2)
+#     syl=round(total_m2/total_m1*100,2)
+#     print('总奖金为:%s,总花费为:%s,收益率为:%s%%'%(total_m2,total_m1,syl))
+#
+#     arr = np.array(m3)
+#     arr1 = arr[arr >= syl]
+#     arr2 = arr1[ (arr1 >= 1300) & (arr1 <= 1550)]
+#     arr3 = arr1[ arr1 > 1550]
+#     sec = len(arr2)
+#     fir = len(arr3)
+#     #print(sorted(arr1))
+#     print('截至目前二等奖的数量为:%s,一等奖的数量为:%s'%(sec,fir))
+import pandas as pd
+from matplotlib import pyplot as plt
 
-    arr = np.array(m3)
-    arr1 = arr[arr >= syl]
-    arr2 = arr1[ (arr1 >= 1300) & (arr1 <= 1550)]
-    arr3 = arr1[ arr1 > 1550]
-    sec = len(arr2)
-    fir = len(arr3)
-    #print(sorted(arr1))
-    print('截至目前二等奖的数量为:%s,一等奖的数量为:%s'%(sec,fir))
+csv_file = 'D:/work2/cc_analy.csv'
+columns=['次数','中奖号码','购买次数','一等奖','二等奖','三等奖','四等奖','五等奖','六等奖', '总花费', '总收益','回报率']
+
+
+
+
+# df = pd.read_csv(csv_file,names=columns)
+# print(df.head(1).T)
+#
+# h1=df['总花费'].sum()
+# h2=df['总收益'].sum()
+#
+# h3=h2/h1
+
+# print(h1,h2,h3)
+
+
+# print(df['二等奖'].sum())
+# print(df['总花费'].sum())
+# print('回报率为:%.2f%%' % (df['总收益'].sum()/df['总花费'].sum()*100))
+
+total_h = 0
+total_s = 0
+i = 0
+x = []
+y = []
+y_t = []
+j_1 = []
+j_2 = []
+j_3 = []
+df = pd.read_csv(csv_file,names=columns,chunksize=100)
+for chunk in df:
+    i += len(chunk)
+    x1 = chunk['总花费'].sum()
+
+    js_1 = chunk['一等奖'].sum()
+    js_2 = chunk['二等奖'].sum()
+    js_3 = chunk['三等奖'].sum()
+
+
+    total_h += x1
+    x2 = chunk['总收益'].sum()
+    total_s += x2
+    x3 = x2 / x1 * 100
+
+    x4 = total_s/total_h*100
+
+    x.append(i)
+    y.append(x3)
+    y_t.append(x4)
+
+    j_1.append(js_1)
+    j_2.append(js_2)
+    j_3.append(js_3)
+
+plt.figure(1)
+plt.subplot(1, 2, 1)
+plt.xlabel("num")#x轴上的名字
+plt.ylabel("money")#y轴上的名字
+
+plt.plot(x, y, label="this period")
+plt.plot(x, y_t, label="arv")
+plt.legend(loc='upper left', bbox_to_anchor=(0.2, 0.95))
+
+#用setp方法可以同时设置多个线条的属性
+
+plt.figure(1)
+plt.subplot(1, 2, 2)
+plt.xlabel("num")#x轴上的名字
+plt.ylabel("j_num")#y轴上的名字
+
+plt.plot(x, j_1, label="first")
+plt.plot(x, j_2, label="second" )
+plt.plot(x, j_3, label="third")
+plt.legend(loc='upper left', bbox_to_anchor=(0.2, 0.95))
+
+plt.show()
