bac7b1d5adad561e7780258bee199f850c198aef
luokesasi <919880391@qq.com>
2021-11-25 18:30:31 +0800

    文本处理

--- ww/Hive/data_clean.py
+++ ww/Hive/data_clean.py
@@ -64,9 +64,7 @@ def keep_last_n_by_dt_hotel(dfs, table, n=1, fl=None):
     history['month'] = history['dt'].apply(lambda x: f"{x.split('-')[0]}-{x.split('-')[1]}")
     history.drop_duplicates(subset=['hotel', 'month'], keep='first', inplace=True)
     exclude = history[['dt', 'hotel']].values.tolist()
-    print("exclude is %s" %exclude)
     g = dfs.groupby('hotel')
-    # print(list(g))
 
     clean_df = pd.DataFrame(columns=['hotel', 'dt'])
     for (k, df) in g.__iter__():
@@ -77,7 +75,6 @@ def keep_last_n_by_dt_hotel(dfs, table, n=1, fl=None):
         clean_df = clean_df.append(df.loc[:, ['dt', 'hotel']])
     clean_df.sort_values(by=['dt', 'hotel'], ascending=True, inplace=True)
     temp_partitions = clean_df.values.tolist()
-    print(temp_partitions)
     temp_partitions = [x for x in temp_partitions if x not in exclude]
     return list(
         map(lambda x: """ALTER TABLE %s DROP IF EXISTS PARTITION (dt='%s', hotel='%s');""" % (table, x[0], x[1]),
@@ -85,7 +82,7 @@ def keep_last_n_by_dt_hotel(dfs, table, n=1, fl=None):
 
 
 
-table_strategy = {'odc.t_fdm_pms_market_stat_daily_ful': ['keep_last_n_by_dt_hotel', {'fl': "~df['dt'].str.endswith('-01')", 'n': 2}]}
+table_strategy = {'odc.t_fdm_pms_market_stat_daily_ful': ['keep_last_n_by_dt_hotel', {'fl': "~df['dt'].str.endswith('-01')", 'n': 3}]}
 def fetch_create_table(table):
     cmd = """hive -e 'show create table %s'""" % table
     (status, output) = run_shell(cmd)
@@ -144,93 +141,24 @@ if __name__ == '__main__':
     for k, v in table_strategy.items():
         print('正在处理: %s' % k)
         partition_fields = ['dt', 'hotel']
-        partitions =[['2021-09-01', 'CNJJN001'], ['2021-09-01', 'CNSZV002'], ['2021-09-01', 'CWH'],['2021-09-03', 'CNJJN001'],
-                       ['2021-09-01', 'CWSW'], ['2021-09-01', 'ESL'], ['2021-09-01', 'FIJ'], ['2021-09-01', 'GSH'],
-                       ['2021-09-01', 'HBKC'], ['2021-09-01', 'HJB'], ['2021-09-01', 'ISL'], ['2021-09-01', 'KHHK'],
-                       ['2021-09-01', 'KHPU'], ['2021-09-01', 'KSL'], ['2021-09-01', 'MAC'], ['2021-09-01', 'RRR'],
-                       ['2021-09-01', 'RSR'], ['2021-09-01', 'SBHI'], ['2021-09-01', 'SEN'], ['2021-09-01', 'SHAR'],
-                       ['2021-09-01', 'SHAS'], ['2021-09-01', 'SHHZ'], ['2021-09-01', 'SLAD'], ['2021-09-01', 'SLB'],
-                       ['2021-09-01', 'SLBK'], ['2021-09-01', 'SLBO'], ['2021-09-01', 'SLBT'], ['2021-09-01', 'SLCB'],
-                       ['2021-09-01', 'SLCC'], ['2021-09-01', 'SLCM'], ['2021-09-01', 'SLCZ'], ['2021-09-01', 'SLDA'],
-                       ['2021-09-01', 'SLDB'], ['2021-09-01', 'SLDQ'], ['2021-09-01', 'SLFM'], ['2021-09-01', 'SLFT'],
-                       ['2021-09-01', 'SLFZ'], ['2021-09-01', 'SLGL'], ['2021-09-01', 'SLH'], ['2021-09-01', 'SLLS'],
-                       ['2021-09-01', 'SLUB'], ['2021-09-01', 'TSHY'], ['2021-09-02', 'CWH'],
-                       ['2021-10-06', 'CNSZV002'], ['2021-10-06', 'CWH'], ['2021-10-06', 'CWSW'], ['2021-10-06', 'ESL'],
-                       ['2021-10-06', 'FIJ'], ['2021-10-06', 'GSH'], ['2021-10-06', 'HBKC'], ['2021-10-06', 'HJB'],
-                       ['2021-10-06', 'ISL'], ['2021-10-06', 'KHHK'], ['2021-10-06', 'KHPU'], ['2021-10-06', 'KSL'],
-                       ['2021-10-06', 'MAC'], ['2021-10-06', 'RRR'], ['2021-10-06', 'RSR'], ['2021-10-06', 'SBHI'],
-                       ['2021-10-06', 'SEN'], ['2021-10-06', 'SHAR'], ['2021-10-06', 'SHAS'], ['2021-10-06', 'SHHZ'],
-                       ['2021-10-06', 'SLAD'], ['2021-10-06', 'SLB'], ['2021-10-06', 'SLBK'], ['2021-10-06', 'SLBO'],
-                       ['2021-10-06', 'SLBT'], ['2021-10-06', 'SLCB'], ['2021-10-06', 'SLCC'], ['2021-10-06', 'SLCM'],
-                       ['2021-10-06', 'SLCZ'], ['2021-10-06', 'SLDA'], ['2021-10-06', 'SLDB'], ['2021-10-06', 'SLDQ'],
-                       ['2021-10-06', 'SLFM'], ['2021-10-06', 'SLFT'], ['2021-10-06', 'SLFZ'], ['2021-10-06', 'SLGL'],
-                       ['2021-10-06', 'SLH'], ['2021-10-06', 'SLHF'], ['2021-10-06', 'SLHH'], ['2021-10-06', 'SLHI'],
-                       ['2021-10-06', 'SLHN'], ['2021-10-06', 'SLIB'], ['2021-10-06', 'SLJ'], ['2021-10-06', 'SLJI'],
-                       ['2021-10-06', 'SLJN'], ['2021-10-06', 'SLKL'], ['2021-10-06', 'SLLS'], ['2021-10-06', 'SLM'],
-                       ['2021-10-06', 'SLMC'], ['2021-10-06', 'SLMZ'], ['2021-10-06', 'SLNB'], ['2021-10-06', 'SLNC'],
-                       ['2021-10-06', 'SLND'], ['2021-10-06', 'SLNJ'], ['2021-10-06', 'SLP'], ['2021-10-06', 'SLPG'],
-                       ['2021-10-06', 'SLPR'], ['2021-10-06', 'SLPU'], ['2021-10-06', 'SLQ'], ['2021-10-06', 'SLQF'],
-                       ['2021-10-06', 'SLQH'], ['2021-10-06', 'SLS'], ['2021-10-06', 'SLSH'], ['2021-10-06', 'SLSN'],
-                       ['2021-10-06', 'SLSZ'], ['2021-10-06', 'SLTJ'], ['2021-10-06', 'SLTN'], ['2021-10-06', 'SLTO'],
-                       ['2021-10-06', 'SLTR'], ['2021-10-06', 'SLTS'], ['2021-10-06', 'SLTY'], ['2021-10-06', 'SLUB'],
-                       ['2021-10-06', 'SLV'], ['2021-10-06', 'SLWU'], ['2021-10-06', 'SLWZ'], ['2021-10-06', 'SLXM'],
-                       ['2021-10-06', 'SLXN'], ['2021-10-06', 'SLYW'], ['2021-10-06', 'SLYZ'], ['2021-10-06', 'SLZ'],
-                       ['2021-10-06', 'SUR'], ['2021-10-06', 'TAH'], ['2021-10-06', 'THAD'], ['2021-10-06', 'THHK'],
-                       ['2021-10-06', 'THKL'], ['2021-10-06', 'THM'], ['2021-10-06', 'THMD'], ['2021-10-06', 'THOG'],
-                       ['2021-10-06', 'THPH'], ['2021-10-06', 'THS'], ['2021-10-06', 'TPE'], ['2021-10-06', 'TSHY']]
-        hdfs_partitions = [['2021-09-01', 'CNJJN001'], ['2021-09-01', 'CNSZV002'], ['2021-09-01', 'CWH'],['2021-09-03', 'CNJJN001'],
-                            ['2021-09-01', 'CWSW'], ['2021-09-01', 'ESL'], ['2021-09-01', 'FIJ'], ['2021-09-01', 'GSH'],
-                            ['2021-09-01', 'HBKC'], ['2021-09-01', 'HJB'], ['2021-09-01', 'ISL'],
-                            ['2021-09-01', 'KHHK'], ['2021-09-01', 'KHPU'], ['2021-09-01', 'KSL'],
-                            ['2021-09-01', 'MAC'], ['2021-09-01', 'RRR'], ['2021-09-01', 'RSR'], ['2021-09-01', 'SBHI'],
-                            ['2021-09-01', 'SEN'], ['2021-09-01', 'SHAR'], ['2021-09-01', 'SHAS'],
-                            ['2021-09-01', 'SHHZ'], ['2021-09-01', 'SLAD'], ['2021-09-01', 'SLB'],
-                            ['2021-09-01', 'SLBK'], ['2021-09-01', 'SLBO'], ['2021-09-01', 'SLBT'],
-                            ['2021-09-01', 'SLCB'], ['2021-09-01', 'SLCC'], ['2021-09-01', 'SLCM'],
-                            ['2021-09-01', 'SLCZ'], ['2021-09-01', 'SLDA'], ['2021-09-01', 'SLDB'],
-                            ['2021-09-01', 'SLDQ'], ['2021-09-01', 'SLFM'], ['2021-09-01', 'SLFT'],
-                            ['2021-09-01', 'SLFZ'], ['2021-09-01', 'SLGL'], ['2021-09-01', 'SLH'],
-                            ['2021-09-01', 'SLLS'], ['2021-09-01', 'SLUB'], ['2021-09-01', 'TSHY'],
-                            ['2021-09-02', 'CWH'], ['2021-10-06', 'CNSZV002'], ['2021-10-06', 'CWH'],
-                            ['2021-10-06', 'CWSW'], ['2021-10-06', 'ESL'], ['2021-10-06', 'FIJ'], ['2021-10-06', 'GSH'],
-                            ['2021-10-06', 'HBKC'], ['2021-10-06', 'HJB'], ['2021-10-06', 'ISL'],
-                            ['2021-10-06', 'KHHK'], ['2021-10-06', 'KHPU'], ['2021-10-06', 'KSL'],
-                            ['2021-10-06', 'MAC'], ['2021-10-06', 'RRR'], ['2021-10-06', 'RSR'], ['2021-10-06', 'SBHI'],
-                            ['2021-10-06', 'SEN'], ['2021-10-06', 'SHAR'], ['2021-10-06', 'SHAS'],
-                            ['2021-10-06', 'SHHZ'], ['2021-10-06', 'SLAD'], ['2021-10-06', 'SLB'],
-                            ['2021-10-06', 'SLBK'], ['2021-10-06', 'SLBO'], ['2021-10-06', 'SLBT'],
-                            ['2021-10-06', 'SLCB'], ['2021-10-06', 'SLCC'], ['2021-10-06', 'SLCM'],
-                            ['2021-10-06', 'SLCZ'], ['2021-10-06', 'SLDA'], ['2021-10-06', 'SLDB'],
-                            ['2021-10-06', 'SLDQ'], ['2021-10-06', 'SLFM'], ['2021-10-06', 'SLFT'],
-                            ['2021-10-06', 'SLFZ'], ['2021-10-06', 'SLGL'], ['2021-10-06', 'SLH'],
-                            ['2021-10-06', 'SLHF'], ['2021-10-06', 'SLHH'], ['2021-10-06', 'SLHI'],
-                            ['2021-10-06', 'SLHN'], ['2021-10-06', 'SLIB'], ['2021-10-06', 'SLJ'],
-                            ['2021-10-06', 'SLJI'], ['2021-10-06', 'SLJN'], ['2021-10-06', 'SLKL'],
-                            ['2021-10-06', 'SLLS'], ['2021-10-06', 'SLM'], ['2021-10-06', 'SLMC'],
-                            ['2021-10-06', 'SLMZ'], ['2021-10-06', 'SLNB'], ['2021-10-06', 'SLNC'],
-                            ['2021-10-06', 'SLND'], ['2021-10-06', 'SLNJ'], ['2021-10-06', 'SLP'],
-                            ['2021-10-06', 'SLPG'], ['2021-10-06', 'SLPR'], ['2021-10-06', 'SLPU'],
-                            ['2021-10-06', 'SLQ'], ['2021-10-06', 'SLQF'], ['2021-10-06', 'SLQH'],
-                            ['2021-10-06', 'SLS'], ['2021-10-06', 'SLSH'], ['2021-10-06', 'SLSN'],
-                            ['2021-10-06', 'SLSZ'], ['2021-10-06', 'SLTJ'], ['2021-10-06', 'SLTN'],
-                            ['2021-10-06', 'SLTO'], ['2021-10-06', 'SLTR'], ['2021-10-06', 'SLTS'],
-                            ['2021-10-06', 'SLTY'], ['2021-10-06', 'SLUB'], ['2021-10-06', 'SLV'],
-                            ['2021-10-06', 'SLWU'], ['2021-10-06', 'SLWZ'], ['2021-10-06', 'SLXM'],
-                            ['2021-10-06', 'SLXN'], ['2021-10-06', 'SLYW'], ['2021-10-06', 'SLYZ'],
-                            ['2021-10-06', 'SLZ'], ['2021-10-06', 'SUR'], ['2021-10-06', 'TAH'], ['2021-10-06', 'THAD'],
-                            ['2021-10-06', 'THHK'], ['2021-10-06', 'THKL'], ['2021-10-06', 'THM'],
-                            ['2021-10-06', 'THMD'], ['2021-10-06', 'THOG'], ['2021-10-06', 'THPH'],
-                            ['2021-10-06', 'THS'], ['2021-10-06', 'TPE'], ['2021-10-06', 'TSHY']]
+        partitions =[['2021-09-03', 'CWH'], ['2021-10-03', 'CWH'], ['2021-09-01', 'CWH'],['2021-10-16', 'CWH']
+        ,['2021-10-04', 'CWH'], ['2021-10-05', 'CWH']
+            ,['2021-09-05', 'CWH'],['2021-09-02', 'CWH'],['2021-10-06', 'CWH'], ['2021-10-01', 'CWH']]
+        partitions=sorted(partitions)
+        hdfs_partitions = [['2021-09-01', 'CWH'],['2021-09-02', 'CWH'], ['2021-10-01', 'CWH']
+                            ,['2021-09-03', 'CWH'],['2021-10-03', 'CWH'], ['2021-10-04', 'CWH'],['2021-10-05', 'CWH'],['2021-10-16', 'CWH']]
+        hdfs_partitions=sorted(hdfs_partitions)
+        print(hdfs_partitions)
         partitions_df = pd.DataFrame(partitions, columns=partition_fields)
         hdfs_partitions_df = pd.DataFrame(hdfs_partitions, columns=partition_fields)
         hdfs_partitions_df = hdfs_partitions_df.drop_duplicates(subset=partition_fields, keep='first')
         merge_df = pd.merge(partitions_df, hdfs_partitions_df, on=partition_fields)
 
-        print("partitions_df is %s"%partitions_df)
-        print("hdfs_partitions_df is %s" % hdfs_partitions_df)
-        print("merge_df is %s" % merge_df)
+        # print("partitions_df is %s"%partitions_df)
+        # print("hdfs_partitions_df is %s" % hdfs_partitions_df)
+        # print("merge_df is %s" % merge_df)
 
-
+        pd.set_option('display.max_rows', None)
         f = eval(v[0])
         print(v[1])
         clean_ddl = f(merge_df, k, **v[1])
