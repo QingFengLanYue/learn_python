5fa0a166f9a16223376bb135b7501c1fd01e9fab
luokesasi <919880391@qq.com>
2022-08-19 18:27:54 +0800

    111

--- ww/NetWork/unzip_file.py
+++ ww/NetWork/unzip_file.py
@@ -3,6 +3,7 @@ from typing import List
 from tqdm import tqdm
 from itertools import chain
 from zipfile import ZipFile
+from rarfile import RarFile
 
 start = time.time()
 
@@ -14,9 +15,8 @@ start = time.time()
 #
 # dictionaries.extend(['.com', 'www.'])    # 添加自定义的字符集
 
-dictionaries = list('6jia12345')
-
-file_name = 'D:/BaiduNetdiskDownload/办公软件/测试2.zip'
+dictionaries = list('123456970jJIAXQiasxq')
+file_name = 'C:/Users/xiqiang.jia/Desktop/杂项/pojie.rar'
 
 
 def all_passwd(dictionaries: List[str], maxlen: int):
@@ -34,24 +34,27 @@ def all_passwd(dictionaries: List[str], maxlen: int):
     yield from helper([0] * maxlen, 0, maxlen)
 
 
-zfile = ZipFile(file_name, 'r')  # 很像open
+if file_name.endswith('.zip'):
+    fp = ZipFile(file_name, 'r')
+elif file_name.endswith('.rar'):
+    fp = RarFile(file_name, 'r')
 
 
-def extract(zfile: ZipFile, pwd: str) -> bool:
-    # zfile: 一个ZipFile类, pwd: 密码
+def extract(fp, pwd: str) -> bool:
     try:
-        zfile.extractall(path='.', pwd=pwd.encode('utf-8'))  # 密码输入错误的时候会报错
-        now = time.time()  # 故使用 try - except 语句
-        print(f"Password is: {pwd}")  # 将正确的密码输出到控制台
+        # print(pwd)
+        fp.extractall(path='.', pwd=pwd.encode('utf-8'))
+        now = time.time()
+        print(f"Password is: {pwd}")
         return True
-    except:
+    except Exception:
         return False
     # 用 bool 类型的返回值告诉主程序是否破解成功 (意思就是返回 True 了以后就停止)
 
 
-lengths = [8]  # 密码长度
+lengths = [3]  # 密码长度
 total = sum(len(dictionaries) ** k for k in lengths)  # 密码总数
 
 for pwd in tqdm(chain.from_iterable(all_passwd(dictionaries, maxlen) for maxlen in lengths), total=total):
-    if extract(zfile, pwd):  # 记得extract函数返回的是bool类型的哦
+    if extract(fp, pwd):  # 记得extract函数返回的是bool类型的哦
         break
