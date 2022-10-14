6becabf21d9605b882d023445a9033921e89bca0
luokesasi <919880391@qq.com>
2022-10-14 10:01:18 +0800

    fixup! Revert "文本处理"

--- python400/Python_03/tank/tank03.py
+++ python400/Python_03/tank/tank03.py
@@ -9,8 +9,9 @@ import random
 import pygame
 import time
 import os
-SCREEN_WIDTH = 740
-SCREEN_HEIGHT = 370
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 200
 BG_COLOR = pygame.Color(0, 0, 0)
 TEXT_COLOR = pygame.Color(255, 0, 0)
 SPEED = 10
@@ -19,9 +20,9 @@ SPEED = 10
 class MainGame:
     window = None
     my_tank = None
-    enemyTankList=[]
+    enemyTankList = []
     enemyTankCount = 5
-
+    myBulletlist=[]
     def __init__(self):
         pass
 
@@ -29,16 +30,17 @@ class MainGame:
         pygame.display.init()
 
         MainGame.window = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-        MainGame.my_tank = Tank(400, 250)
+        MainGame.my_tank = Tank(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         self.creatEnemyTank()
         pygame.display.set_caption('坦克大战1.0')
         while True:
             time.sleep(0.05)
             self.getEvent()
             MainGame.window.fill(BG_COLOR)
-            MainGame.window.blit(self.getTextSufacr(f'敌方坦克剩余数量:{6}'), (10, 10))
-            MainGame.my_tank.displaceTank()
+            MainGame.window.blit(self.getTextSufacr(f'敌方坦克剩余数量:{len(MainGame.enemyTankList)}'), (10, 10))
+            MainGame.my_tank.displayTank()
             self.biltEnemyTank()
+            self.biltMyBullet()
             if not MainGame.my_tank.stop:
                 MainGame.my_tank.move()
             pygame.display.update()
@@ -46,19 +48,26 @@ class MainGame:
     def endGame(self):
         print('退出')
         exit()
+
     def creatEnemyTank(self):
         top = 10
         for i in range(MainGame.enemyTankCount):
-            left = random.randint(0,740)
-            speed = random.randint(1,4)
-            enemy = EnemyTank(left,top,speed)
-            print(left,top,speed)
+            left = random.randint(0, SCREEN_WIDTH)
+            speed = random.randint(1, 4)
+            enemy = EnemyTank(left, top, speed)
+            print(left, top, speed)
             MainGame.enemyTankList.append(enemy)
+
     def biltEnemyTank(self):
         for enemyTank in MainGame.enemyTankList:
             # print(enemyTank)
-            enemyTank.displaceTank()
+            enemyTank.displayTank()
+            enemyTank.randMove()
 
+    def biltMyBullet(self):
+        for myBullet in MainGame.myBulletlist:
+            # print(enemyTank)
+            myBullet.displayBullet()
     def getTextSufacr(self, text):
         pygame.font.init()
         # print(pygame.font.get_fonts())
@@ -93,8 +102,10 @@ class MainGame:
                     print('向下移动')
                 elif event.key == pygame.K_SPACE:
                     print('bububu')
+                    myBullet=Bullet(MainGame.my_tank)
+                    MainGame.myBulletlist.append(myBullet)
             if event.type == pygame.KEYUP:
-                if event.key in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT,pygame.K_RIGHT):
+                if event.key in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT):
                     MainGame.my_tank.stop = True
 
 
@@ -118,7 +129,7 @@ class Tank:
             self.rect.left -= self.speed
         elif self.direction == 'R' and self.rect.left + self.rect.width < SCREEN_WIDTH:
             self.rect.left += self.speed
-        elif self.direction == 'U' and self.rect.top-self.speed > 0:
+        elif self.direction == 'U' and self.rect.top - self.speed > 0:
             self.rect.top -= self.speed
         elif self.direction == 'D' and self.rect.top + self.rect.width < SCREEN_HEIGHT:
             self.rect.top += self.speed
@@ -126,27 +137,30 @@ class Tank:
     def shot(self):
         pass
 
-    def displaceTank(self):
+    def displayTank(self):
         self.image = self.images[self.direction]
         MainGame.window.blit(self.image, self.rect)
 
 
 class EnemyTank(Tank):
-    def __init__(self,left,top,speed):
+    def __init__(self, left, top, speed):
+        super().__init__(left, top)
         self.images = {'U': pygame.image.load('img/tankU.gif'),
                        'D': pygame.image.load('img/tankD.gif'),
                        'L': pygame.image.load('img/tankL.gif'),
                        'R': pygame.image.load('img/tankR.gif')
                        }
-        self.direction= self.randDirection()
+        self.direction = self.randDirection()
         self.image = self.images[self.direction]
         self.rect = self.image.get_rect()
         self.rect.left = left
         self.rect.top = top
         self.speed = speed
         self.flag = True
+        self.step = 10
+
     def randDirection(self):
-        num = random.randint(1,4)
+        num = random.randint(1, 4)
         if num == 1:
             return 'U'
         elif num == 2:
@@ -156,9 +170,41 @@ class EnemyTank(Tank):
         elif num == 4:
             return 'R'
 
+    def randMove(self):
+        if self.step <= 0:
+            self.direction = self.randDirection()
+            self.step = 20
+        else:
+            self.move()
+            self.step -= 1
+
+class Bullet:
+    def __init__(self,tank):
+        self.image = pygame.image.load('img/enemybullet.png')
+        self.rect = self.image.get_rect()
+        self.direction = tank.direction
+        print(self.direction)
+        if self.direction == 'U':
+            self.rect.left = tank.rect.left + tank.rect.width/2 - self.rect.width*1.2
+            self.rect.top = tank.rect.top - self.rect.height
+        elif self.direction == 'D':
+            self.rect.left = tank.rect.left + tank.rect.width / 2 - self.rect.width*1.75
+            self.rect.top = tank.rect.top + tank.rect.height + self.rect.height
+            print(f'坦克:{tank.rect.top},{tank.rect.left},{tank.rect.width},{tank.rect.height}')
+            print(f'子弹:{self.rect.top},{self.rect.left},{self.rect.width},{self.rect.height}')
+        elif self.direction == 'L':
+            self.rect.left = tank.rect.left - self.rect.width / 2
+            self.rect.top = tank.rect.top + tank.rect.width / 2 - self.rect.height
+        elif self.direction == 'R':
+            self.rect.left = tank.rect.left + tank.rect.width
+            self.rect.top = tank.rect.top + tank.rect.width/2 - self.rect.height
+        self.speed = 6
+    def move(self):
+        pass
+
+    def displayBullet(self):
+        MainGame.window.blit(self.image,self.rect)
 
 if __name__ == '__main__':
     x = MainGame()
     x.starGame()
-
-
